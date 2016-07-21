7407e9149fa10869502165d773dcb4720e944f70
Lars <leetruff@gmail.com>
2016-07-21 20:55:13 +0200

    Grobes Setup

--- core/src/com/ruffles/boostergame/MyGdxGame.java
+++ core/src/com/ruffles/boostergame/MyGdxGame.java
@@ -1,28 +1,23 @@
 package com.ruffles.boostergame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
+	static int V_WIDTH = 740;
+	static int V_HEIGHT = 360;
+	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		System.out.println("Minor Text fixes");
+		setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 }
